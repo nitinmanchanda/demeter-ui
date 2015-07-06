e3526a0df38458034813d6122e8b535f285388dc
nitinmanchanda <nitinmanchanda.pucsd@gmail.com>
2015-07-06 12:50:59 +0530

    formatting changes

--- config.py
+++ config.py
@@ -5,7 +5,11 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 database_name = 'demeter'
 
+
 class Config:
+    def __init__(self):
+        pass
+
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 
@@ -13,17 +17,21 @@ class Config:
     def init_app(app):
         pass
 
+
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sqladmin:Qwedsazxc123@staging-mysql.c0wj8qdslqom.ap-southeast-1.rds.amazonaws.com/' + database_name
+
 
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sqladmin:Qwedsazxc123@staging-mysql.c0wj8qdslqom.ap-southeast-1.rds.amazonaws.com/' + database_name
 
+
 class ProductionConfig(Config):
     PRODUCTION = True
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sqladmin:Qwedsazxc123@staging-mysql.c0wj8qdslqom.ap-southeast-1.rds.amazonaws.com/' + database_name
+
 
 config = {
     'development': DevelopmentConfig,
